775be235b3bebe35c163042918f6353e97363fab
Skitovich Ruslan <zloycvetok@gmail.com>
2020-08-17 22:04:23 +0300

    Код

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        int currentBill = 0;
-        int transferSum = 2_147_483_648;
+        int currentBill = 2_000_000_000;
+        int transferSum = 500_000_000;
         int totalBill = currentBill + transferSum;
         System.out.println(totalBill);
 }
