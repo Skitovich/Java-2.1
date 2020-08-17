af4447db9495a6775a64e4ab1d31d83d63b0f13f
Skitovich Ruslan <zloycvetok@gmail.com>
2020-08-17 22:01:22 +0300

    Код приложения

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int currentBill = 0;
+        int transferSum = 2_147_483_648;
+        int totalBill = currentBill + transferSum;
+        System.out.println(totalBill);
+}
+}
